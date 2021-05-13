30079c635126c09fb82e4b647511e3af6a49df95
b3-bogusz <b3.bogusz@gmail.com>
2021-05-13 19:24:24 +0200

    add error and success handlers

--- src/js/controller.js
+++ src/js/controller.js
@@ -23,10 +23,11 @@ const controlRecipes = async function() {
     recipeView.render(model.state.recipe);
 
   } catch (err) {
-    alert(err)
+    recipeView.renderError();
   }
 }
 
-// ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, controlRecipes));
-window.addEventListener('hashchange', controlRecipes);
-window.addEventListener('load', controlRecipes);
+const init = function() {
+  recipeView.addHandlerRender(controlRecipes);
+}
+init();
