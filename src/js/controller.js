2efb06c0777213dca60c223cd43a36ba478959bc
b3-bogusz <b3.bogusz@gmail.com>
2021-05-13 21:24:42 +0200

    add search results render

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,18 +1,21 @@
 import * as model from './model.js'
 import recipeView from './views/recipeView';
+import searchView from './views/searchView';
+import resultView from './views/resultsView';
 
 import 'core-js/stable';                //npm package for polyfilling
 import 'regenerator-runtime/runtime';   // npm package for polyfilling async/await
 
-const recipeContainer = document.querySelector('.recipe');
-
+if (module.hot) {
+  module.hot.accept();
+}
 
 // https://forkify-api.herokuapp.com/v2
 ///////////////////////////////////////
 const controlRecipes = async function() {
   try {
     const id = window.location.hash.slice(1);
-    console.log(id);
+
     if (!id) return;
     recipeView.renderSpinner();
 
@@ -27,7 +30,27 @@ const controlRecipes = async function() {
   }
 }
 
+const controlSearchResults = async function() {
+  try {
+    resultView.renderSpinner();
+
+    // Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // Load search results
+    await model.loadSearchResults(query);
+
+    // Render results
+    resultView.render(model.state.search.results);
+
+  } catch(err) {
+    console.log(err);
+  }
+}
+
 const init = function() {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 }
 init();
