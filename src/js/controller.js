62ccb10bb3ec1be74061b51d73c5cb9f2170e161
b3-bogusz <b3.bogusz@gmail.com>
2021-05-14 13:32:13 +0200

    add pagination on search results

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,13 +2,15 @@ import * as model from './model.js'
 import recipeView from './views/recipeView';
 import searchView from './views/searchView';
 import resultView from './views/resultsView';
+import paginationView from './views/paginationView';
 
 import 'core-js/stable';                //npm package for polyfilling
-import 'regenerator-runtime/runtime';   // npm package for polyfilling async/await
+import 'regenerator-runtime/runtime';
+import { getSearchResultsPage } from './model.js';   // npm package for polyfilling async/await
 
-if (module.hot) {
-  module.hot.accept();
-}
+// if (module.hot) {
+//   module.hot.accept();
+// }
 
 // https://forkify-api.herokuapp.com/v2
 ///////////////////////////////////////
@@ -42,15 +44,27 @@ const controlSearchResults = async function() {
     await model.loadSearchResults(query);
 
     // Render results
-    resultView.render(model.state.search.results);
+    resultView.render(model.getSearchResultsPage());
+
+    // Render initial pagination buttons
+    paginationView.render(model.state.search);
 
   } catch(err) {
     console.log(err);
   }
 }
 
+const controlPagination = function(goToPage) {
+  // Render new results
+  resultView.render(model.getSearchResultsPage(goToPage));
+
+  // Render new pagination buttons
+  paginationView.render(model.state.search);
+}
+
 const init = function() {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.adddHandlerClick(controlPagination);
 }
 init();
